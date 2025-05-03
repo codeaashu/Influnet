d6b96b06a7c4eaebf1b5c2b09775a636a1ecbaba
ashutoshpythoncs@gmail.com <ashutoshpythoncs@gmail.com>
2025-05-03 22:28:11 +0530

    Influnet

--- server/src/types/influencer-category.ts
+++
@@ -1,8 +0,0 @@
-export interface InfluencerCategoryAttributes {
-  id: string;
-  influencer_id: string;
-  category_name: string;
-}
-
-export interface InfluencerCategoryCreationAttributes
-  extends Omit<InfluencerCategoryAttributes, "id"> {}
--- server/src/types/social-media-platform.ts
+++
@@ -1,9 +0,0 @@
-export interface SocialMediaPlatformAttributes {
-  id?: string; // Optional because Sequelize auto-generates it
-  platform_name: string;
-  platform_icon_url: string;
-  domain_name: string;
-}
-
-export interface SocialMediaPlatformCreationAttributes
-  extends Omit<SocialMediaPlatformAttributes, "id"> {}
